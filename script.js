7fd914f690df3a3d6d92bf47d28942d1e2011cb3
Miguel Ángel Prosper <miguelangel.prosper@gmail.com>
2026-03-19 16:51:42 +0100

    add hasher

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // === BUILTINS ===
 function hex(value, pad) {
     return value.toString(16).padStart(pad || 0, '0')
@@ -151,6 +153,183 @@ class Struct {
         for (let offset = 0; offset + this.size <= bytes.length; offset += this.size) {
             yield this.unpackFrom(bytes, offset);
         }
+    }
+}
+
+// === HASH ===
+/* Based on https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf */
+class Sha256 {
+    static blockSize = 64;
+    static digestSize = 32;
+    static #K = new Uint32Array([
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    ]);
+
+    static #getUint32BE(x, offset) {
+        return (
+            (x[offset + 0] << 24) |
+            (x[offset + 1] << 16) |
+            (x[offset + 2] << 8) |
+            (x[offset + 3] << 0)
+        );
+    }
+
+    static #ROTR(x, n) {
+        return (x >>> n) | (x << (32 - n));
+    }
+
+    static #Ch(x, y, z) {
+        return (x & y) ^ (~x & z);
+    }
+
+    static #Maj(x, y, z) {
+        return (x & y) ^ (x & z) ^ (y & z);
+    }
+
+    static #S0(x) {
+        return Sha256.#ROTR(x, 2) ^ Sha256.#ROTR(x, 13) ^ Sha256.#ROTR(x, 22);
+    }
+
+    static #S1(x) {
+        return Sha256.#ROTR(x, 6) ^ Sha256.#ROTR(x, 11) ^ Sha256.#ROTR(x, 25);
+    }
+
+    static #s0(x) {
+        return Sha256.#ROTR(x, 7) ^ Sha256.#ROTR(x, 18) ^ (x >>> 3);
+    }
+
+    static #s1(x) {
+        return Sha256.#ROTR(x, 17) ^ Sha256.#ROTR(x, 19) ^ (x >>> 10);
+    }
+
+    static #compute(M, H, W) {
+        // 1. Prepare the message schedule, W
+        for (let i = 0; i <= 15; i++) W[i] = Sha256.#getUint32BE(M, i * 4);
+        for (let i = 16; i <= 63; i++) W[i] = W[i - 16] + Sha256.#s0(W[i - 15]) + W[i - 7] + Sha256.#s1(W[i - 2]);
+
+        // 2. Initialize the eight working variables, a, b, c, d, e, f, g, and h, with the (i-1)st hash value
+        let [a, b, c, d, e, f, g, h] = H;
+
+        // 3. For t=0 to 63
+        for (let t = 0; t <= 63; t++) {
+            const T1 = h + Sha256.#S1(e) + Sha256.#Ch(e, f, g) + Sha256.#K[t] + W[t];
+            const T2 = Sha256.#S0(a) + Sha256.#Maj(a, b, c);
+            h = g;
+            g = f;
+            f = e;
+            e = d + T1;
+            d = c;
+            c = b;
+            b = a;
+            a = T1 + T2;
+        }
+
+        // 4. Compute the ith intermediate hash value H[i]
+        H[0] += a;
+        H[1] += b;
+        H[2] += c;
+        H[3] += d;
+        H[4] += e;
+        H[5] += f;
+        H[6] += g;
+        H[7] += h;
+    }
+
+    #length = 0;
+    #M = new Uint8Array(Sha256.blockSize);
+    #W = new Uint32Array(64);
+    #H = new Uint32Array([
+        0x6a09e667, 0xbb67ae85,
+        0x3c6ef372, 0xa54ff53a,
+        0x510e527f, 0x9b05688c,
+        0x1f83d9ab, 0x5be0cd19
+    ]);
+
+    constructor(bytes) {
+        if (bytes !== undefined) this.update(bytes);
+    }
+
+    get #i() {
+        return this.#length % Sha256.blockSize;
+    }
+
+    update(bytes) {
+        let offset = 0;
+        let size, chunk;
+
+        // if data in buffer and can fill buffer, fill and compute
+        size = Sha256.blockSize - this.#i;
+        if (this.#i > 0 && bytes.length >= size) {
+            chunk = bytes.subarray(offset, offset + size);
+            this.#M.set(chunk, this.#i);
+            Sha256.#compute(this.#M, this.#H, this.#W);
+            offset += size;
+        }
+
+        // while full block available, view and compute
+        size = Sha256.blockSize;
+        while (offset <= (bytes.length - size)) {
+            chunk = bytes.subarray(offset, offset + size);
+            Sha256.#compute(chunk, this.#H, this.#W);
+            offset += size;
+        }
+
+        // if remainder, add to buffer
+        size = bytes.length - offset;
+        this.#length += offset;
+        if (size > 0) {
+            chunk = bytes.subarray(offset, offset + size);
+            this.#M.set(chunk, this.#i);
+            offset += size;
+        }
+        this.#length += size;
+    }
+
+    digest() {
+        let view;
+        const hasher = this.copy();
+
+        // padding
+        const [min, max] = [1 + 8, Sha256.blockSize];
+        const length = max - (hasher.#i + min);
+        const padding = new Uint8Array(min + ((length + max) % max));
+        view = new DataView(padding.buffer);
+        view.setUint8(0, 0b10000000);
+        view.setBigUint64(view.byteLength - 8, BigInt(hasher.#length) * 8n, false);
+        hasher.update(padding);
+
+        // digest
+        const digest = new Uint8Array(Sha256.digestSize);
+        view = new DataView(digest.buffer);
+        for (let i = 0; i < hasher.#H.length; i++) view.setUint32(i * 4, hasher.#H[i], false);
+        return digest;
+    }
+
+    hexdigest() {
+        return Array.from(this.digest()).map(b => b.toString(16).padStart(2, '0')).join('');
+    }
+
+    copy() {
+        const hasher = new Sha256();
+        hasher.#length = this.#length;
+        hasher.#M.set(this.#M);
+        hasher.#H.set(this.#H);
+        return hasher;
     }
 }
 
